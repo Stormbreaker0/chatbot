b31b6018b74168ad308b6316881c187244a3d6a1
VIRGIL <virgilnjanpou0@gmail.com>
2025-01-03 15:24:58 +0100

    update startbot

--- startbot.sh
+++ startbot.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Navigate to the directory containing your Python application
-#cd /path/to/your/python/app
+cd /home/virgil/Desktop/chatbot || { echo "Failed to navigate to /home/virgil/Desktop/chatbot"; exit 1; }
 
 # Create and activate the virtual environment (if it doesn't exist)
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    python3 -m venv venv || { echo "Failed to create virtual environment"; exit 1; }
 fi
-source venv/bin/activate
 
-# Check and install packages from requirements.txt if they are not already installed
-pip check || pip install -r requirements.txt 
+# Activate the virtual environment
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 
-echo "starting bot..."
+pip install -r requirements.txt
+
+echo "starting the bot..."
 # Run the Python application
-python chatbot.py
+python chatbot.py || { echo "Failed to start chatbot"; exit 1; }
 
 # Deactivate the virtual environment
-deactivate
+deactivate || { echo "Failed to deactivate virtual environment"; exit 1; }
 
+echo "Script completed successfully"
